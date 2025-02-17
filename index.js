ba65c0207b7754c2e36a49b488efa2d8fbe87968
ipadpro-clewaka <gomimaedakitaare@gmail.com>
2025-02-17 19:48:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -83,61 +83,6 @@ app.post('/logout', (req, res) => {
     return res.redirect('/login');
 });
 
-//埋め込み用(取得)
-app.get('/embed/:id/', async (req, res) => {
-if (req.cookies.massiropass !== 'ok') {
-    const videoId = req.params.id;
-    const server = req.query.server || '0';
-    const serverUrls = {
-        '0': [
-        'https://natural-voltaic-titanium.glitch.me',
-        'https://wtserver3.glitch.me',
-        'https://wtserver1.glitch.me',
-        'https://wtserver2.glitch.me',
-        ],
-        '1': 'https://wataamee.glitch.me',
-        '2': 'https://watawatawata.glitch.me',
-        '3': 'https://amenable-charm-lute.glitch.me',
-        '4': 'https://wtserver2.glitch.me',
-        '5': 'https://wtserver1.glitch.me',
-        "6": "https://battle-deciduous-bear.glitch.me",
-        "7": 'https://productive-noon-van.glitch.me',
-	"8": 'https://balsam-secret-fine.glitch.me',
-    };
-
-    let baseUrl;
-    if (server === '0') {
-        const randomIndex = Math.floor(Math.random() * serverUrls['0'].length);
-        baseUrl = serverUrls['0'][randomIndex];
-    } else {
-        baseUrl = serverUrls[server] || 'https://wtserver1.glitch.me';
-    }
-  
-    if (!/^[a-zA-Z0-9_-]{11}$/.test(videoId)) {
-        return res.status(400).send('videoIDが正しくありません');
-    }
-
-    const cookies = parseCookies(req);
-    const wakames = cookies.wakametubeumekomi === 'true';
-    if (wakames) {
-        return res.redirect(`/umekomi/${videoId}`);
-    }
-    try {
-        console.log(baseUrl);
-        const response = await axios.get(`${baseUrl}/api/${videoId}`);
-        const videoData = response.data;
-        console.log(videoData);
-	res.render(`login/embed.ejs`,{ videoData, videoId, baseUrl });
-  } catch (error) {
-        res.status(500).render(`login/matte`, { 
-      videoId, baseUrl,
-      error: '動画を取得できません', 
-      details: error.message 
-    });
-  }
-}
-});
-
 //以前の外観
 app.get('/w-ymod/:id/', async (req, res) => {
     const videoId = req.params.id;
